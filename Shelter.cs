96f2ae9c2183c2b9d122167eeea456c94db4cd71
Ivan Klimovskiy <klimoffsky27@gmail.com>
2026-05-20 12:22:44 +0300

    Раскидать файлы по папкам

--- Shelter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConsoleCityRPG;
-
-public class Shelter : Building
-{
-    public Shelter(int coordinateX, int coordinateY) : base(coordinateX, coordinateY)
-    {
-        Role = "Shelter";
-    }
-}
--- Shop.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConsoleCityRPG;
-
-public class Shop : Building
-{
-    public Shop(int coordinateX, int coordinateY) : base(coordinateX, coordinateY)
-    {
-        Role = "Shop";
-    }
-}
