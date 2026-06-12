796941747fd10800f003ebd7c98c1e8c75089e68
Ivan Klimovskiy <klimoffsky27@gmail.com>
2026-06-12 15:36:34 +0300

    Обновление

--- obj/Debug/net8.0/ConsoleCityRPG.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleCityRPG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleCityRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c80869d44c649e7a7e88cde391fd4928f3887b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1d4a57432358ed315f0016c1448664fc2ab4aa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleCityRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleCityRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
